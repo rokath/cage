e339d9db65365ca442c8a3ed470672ffc50542fe
Tea Age <baumtec@seerose.net>
2020-04-14 18:33:18 +0200

    intermediate state, seems to work

--- cage.go
+++ cage.go
@@ -4,56 +4,88 @@ import (
 	"bytes"
 	"io"
 	"os"
-	"strings"
 	"time"
 )
 
 type Container struct {
-	backupStdout *os.File
-	writerStdout *os.File
-	backupStderr *os.File
-	writerStderr *os.File
+	origStdout   *os.File // old
+	writerStdout *os.File // new
 
-	data    string
-	channel chan string
+	origStderr   *os.File // old
+	writerStderr *os.File // new
 
-	Data []string
+	sChannel chan string // channel for os.Stdout messages
+	eChannel chan string // channel for os.Stderr messages
+
+	//data   string
+	//Data []string
+
+	lfHandle *os.File // logfile handle
 }
 
-func Start() *Container {
+// Start does append all output parallel into a logfile with name fn
+func Start(fn string) *Container {
+
+	// open logfile
+	lfH, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		//log.Fatalf("error opening file %s: %v", fn, err)
+		fn = "off"
+		return nil
+	}
+
+	// open pipes
 	rStdout, wStdout, _ := os.Pipe()
 	rStderr, wStderr, _ := os.Pipe()
+
+	// create container
 	c := &Container{
-		backupStdout: os.Stdout,
+		origStdout:   os.Stdout,
 		writerStdout: wStdout,
 
-		backupStderr: os.Stderr,
+		origStderr:   os.Stderr,
 		writerStderr: wStderr,
 
-		channel: make(chan string),
-	}
-	os.Stdout = c.writerStdout
-	os.Stderr = c.writerStderr
+		sChannel: make(chan string),
+		eChannel: make(chan string),
 
-	go func(out chan string, readerStdout *os.File, readerStderr *os.File) {
+		lfHandle: lfH,
+	}
+
+	// re-direct
+	os.Stdout = c.writerStdout // all to os.Stdout goees now to c.writerStdout
+	os.Stderr = c.writerStderr // all to os.Stderr goees now to c.writerStderr
+
+	// writing to os.Stdout will go to pipe and come out of rStdout now
+	// writing to os.Stderr will go to pipe and come out of rStderr now
+
+	// create duplication
+	teeOut := io.MultiWriter(c.origStdout, c.lfHandle)
+	teeErr := io.MultiWriter(c.origStderr, c.lfHandle)
+
+	// pipes reader
+	go func(sout, eout chan string, readerStdout *os.File, readerStderr *os.File) {
 		var bufStdout bytes.Buffer
+		// _, _ = io.Copy(teeOut, readerStdout) // CHECK this
 		_, _ = io.Copy(&bufStdout, readerStdout)
 		if bufStdout.Len() > 0 {
-			out <- bufStdout.String()
+			sout <- bufStdout.String()
 		}
 
 		var bufStderr bytes.Buffer
 		_, _ = io.Copy(&bufStderr, readerStderr)
 		if bufStderr.Len() > 0 {
-			out <- bufStderr.String()
+			eout <- bufStderr.String()
 		}
-	}(c.channel, rStdout, rStderr)
+	}(c.sChannel, c.eChannel, rStdout, rStderr)
 
 	go func(c *Container) {
 		for {
 			select {
-			case out := <-c.channel:
-				c.data += out
+			case sout := <-c.sChannel:
+				teeOut.Write([]byte(sout))
+			case eout := <-c.eChannel:
+				teeErr.Write([]byte(eout))
 			}
 		}
 	}(c)
@@ -61,16 +93,19 @@ func Start() *Container {
 	return c
 }
 
+// Stop does return to normal state
 func Stop(c *Container) {
 	_ = c.writerStdout.Close()
 	_ = c.writerStderr.Close()
 	time.Sleep(10 * time.Millisecond)
 
-	os.Stdout = c.backupStdout
-	os.Stderr = c.backupStderr
+	os.Stdout = c.origStdout
+	os.Stderr = c.origStderr
 
-	c.Data = strings.Split(c.data, "\n")
-	if c.Data[len(c.Data)-1] == "" {
-		c.Data = c.Data[:len(c.Data)-1]
-	}
+	c.lfHandle.Close()
+
+	//c.Data = strings.Split(c.data, "\n")
+	//if c.Data[len(c.Data)-1] == "" {
+	//	c.Data = c.Data[:len(c.Data)-1]
+	//}
 }
--- cage_test.go
+++ cage_test.go
@@ -7,26 +7,32 @@ import (
 )
 
 func TestStart(t *testing.T) {
-	c := Start()
+	c := Start("logfile.txt")
 
 	fmt.Println("test")
 	fmt.Println("test2")
 	fmt.Fprintln(os.Stderr, "stderr error")
 
 	Stop(c)
+	/*
 
-	fmt.Println(c.Data)
+		test
+		test2
+		stderr error
 
-	if len(c.Data) != 3 {
-		t.Error("Data length should be 3")
-	}
-	if c.Data[0] != "test" {
-		t.Errorf("First line should be 'test', instead of %s", c.Data[0])
-	}
-	if c.Data[1] != "test2" {
-		t.Errorf("Second line should be 'test2', instead of %s", c.Data[1])
-	}
-	if c.Data[2] != "stderr error" {
-		t.Errorf("Third line should be 'stderr error', instead of %s", c.Data[2])
-	}
+
+				fmt.Println(c.Data)
+
+				if len(c.Data) != 3 {
+					t.Error("Data length should be 3")
+				}
+				if c.Data[0] != "test" {
+					t.Errorf("First line should be 'test', instead of %s", c.Data[0])
+				}
+				if c.Data[1] != "test2" {
+					t.Errorf("Second line should be 'test2', instead of %s", c.Data[1])
+				}
+				if c.Data[2] != "stderr error" {
+					t.Errorf("Third line should be 'stderr error', instead of %s", c.Data[2])
+				}*/
 }
